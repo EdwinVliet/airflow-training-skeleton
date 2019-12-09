3d83f7390d977eb369032a7a7cff445b30d45f3c
EdwinVliet <edwinj92@hotmail.com>
2019-12-09 11:46:45 +0100

    Create dag_1_training_scheduled.py

---
+++ dags/dag_1_training_scheduled.py
@@ -0,0 +1,47 @@
+from datetime import timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='dag_1_training_scheduled_id',
+    default_args=args,
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=60),
+    'start_date': datetime(2019, 12, 1),
+    'schedule_interval': '@daily',
+)
+
+task_1 = DummyOperator(
+    task_id='run_task_1',
+    dag=dag,
+)
+
+task_2 = DummyOperator(
+    task_id='run_task_2',
+    dag=dag,
+)
+
+task_3 = DummyOperator(
+    task_id='run_task_3',
+    dag=dag,
+)
+
+task_4 = DummyOperator(
+    task_id='run_task_4',
+    dag=dag,
+)
+
+task_5 = DummyOperator(
+    task_id='run_task_5',
+    dag=dag,
+)
+
+task_1 >> task_2 >> [task_3, task_4] >> task_5
